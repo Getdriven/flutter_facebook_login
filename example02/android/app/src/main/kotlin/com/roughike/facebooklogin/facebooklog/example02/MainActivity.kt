2bea648a2daa99079011adf1a7f8187dac147203
Glenn Ruysschaert <glenn@getdriven.app>
2020-09-01 13:20:30 +0200

    Remove example 2

--- example02/android/app/src/main/kotlin/com/roughike/facebooklogin/facebooklog/example02/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.roughike.facebooklogin.facebooklog.example02
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- example02/https_cert.sh
+++
@@ -1,2 +0,0 @@
-# Google Chrome chrome://flags/#allow-insecure-localhost
-openssl req -new -x509 -keyout localhost.pem -out localhost.pem -days 365 -nodes
